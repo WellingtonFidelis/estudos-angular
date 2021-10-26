3bebda10a1cdbdb41da38f3a49a79321d6fe6507
WELLINGTON FIDELIS DA SILVEIRA <wellingtonf20@gmail.com>
2021-10-25 22:21:29 -0300

    Adding loading spinner.

--- 02.angular-spring-crud/crud-angular/src/app/courses/courses/courses.component.ts
+++ 02.angular-spring-crud/crud-angular/src/app/courses/courses/courses.component.ts
@@ -10,7 +10,7 @@ import { Observable } from 'rxjs';
   styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent implements OnInit {
-  courses: Observable<Course[]>;
+  courses$: Observable<Course[]>;
 
   displayedColumns = ['name', 'category'];
 
@@ -19,7 +19,7 @@ export class CoursesComponent implements OnInit {
   constructor(private coursesService: CoursesService) {
     // this.courses = [];
     // this.coursesService = new CoursesService();
-    this.courses = this.coursesService.list();
+    this.courses$ = this.coursesService.list();
   }
 
   ngOnInit(): void {}
