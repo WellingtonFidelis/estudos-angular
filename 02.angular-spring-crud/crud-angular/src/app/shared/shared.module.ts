71269e0bcfb4763bc8e2f6b0f26419c6ac91d563
WellingtonFidelis <wellingtonf20@gmail.com>
2021-11-13 15:48:52 -0300

    Development icon usign pipe

--- 02.angular-spring-crud/crud-angular/src/app/shared/shared.module.ts
+++ 02.angular-spring-crud/crud-angular/src/app/shared/shared.module.ts
@@ -3,17 +3,20 @@ import { CommonModule } from '@angular/common';
 
 import { AppMaterialModule } from './app-material/app-material.module';
 import { ErrorDialogComponent } from './components/error-dialog/error-dialog.component';
+import { CategoryPipe } from './pipes/category.pipe';
 
 @NgModule({
   declarations: [
     ErrorDialogComponent,
+    CategoryPipe,
   ],
   imports: [
     CommonModule,
     AppMaterialModule,
   ],
   exports: [
-    ErrorDialogComponent
+    ErrorDialogComponent,
+    CategoryPipe,
   ]
 })
 export class SharedModule { }
